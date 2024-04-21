76b208b44eae387c3b24b2d97158f7d578a52fe3
saket2343 <saketpandey2343@gmail.com>
2024-04-21 21:40:52 +0530

    Project Files

---
+++ Assets/ButtonTest.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using TMPro;
+
+public class ButtonTest : MonoBehaviour
+{
+
+    public TMP_Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        
+
+    }
+    public void ClickButton()
+    {
+        text.text = "You clicked me!";
+    }
+}
---
+++ Assets/TouchTest.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+public class TouchTest : MonoBehaviour
+{
+
+    public GameObject marsPopup;
+    public GameObject earthPopup;
+
+    public TMP_Text infoBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        if (Input.GetMouseButtonDown(0)) //left mouse click
+        {
+            Debug.Log("pressed primary button.");
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.Log("hit");
+                Debug.Log(hit.transform.name + " : " + hit.transform.tag);
+
+                if (hit.transform.tag == "mars")
+                {
+                    /*
+                                        Vector3 pos = hit.point;
+                                        pos.z += 0.25f;
+                                        pos.y += 0.25f;
+                                        Instantiate(marsPopup, pos, transform.rotation); */
+
+                    infoBox.text = "Mars \n Let's go visit!";
+                }
+                if (hit.transform.tag == "earth")
+                {
+                    /*
+                                        Vector3 pos = hit.point;
+                                        pos.z += 0.25f;
+                                        pos.y += 0.25f;
+                                        Instantiate(earthPopup, pos, transform.rotation); */
+                    infoBox.text = "earth \n Let's go visit!";
+                }
+
+                if (hit.transform.tag == "marsInfo")
+                {
+                    Destroy(hit.transform.gameObject);
+                }
+
+                if (hit.transform.tag == "earthsInfo")
+                {
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+}
